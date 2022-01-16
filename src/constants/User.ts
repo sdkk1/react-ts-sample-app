83f1f9ead5c12e9ddec57b1df840f7f20172ed9b
Keita <67583751+sdkk1@users.noreply.github.com>
2022-01-16 14:21:43 +0900

    initial commit

--- src/constants/User.ts
+++ src/constants/User.ts
@@ -5,45 +5,45 @@ import { TableHeader } from 'sampleapp'
  * Backlog APIのユーザー一覧取得APIを想定
  */
 export const USERS_URL =
-	'https://xx.backlog.jp/api/v2/users'
+  'https://xx.backlog.jp/api/v2/users'
 
 /**
  * response users
  * 動作確認用のユーザー一覧
  */
 export const responseUsers = [
-	{
-		id: 1,
-		userId: 'admin',
-		name: 'admin',
-		roleType: 1,
-		lang: 'ja',
-		mailAddress: 'admin@nulab.example'
-	},
-	{
-		id: 2,
-		userId: 'general',
-		name: 'general',
-		roleType: 2,
-		lang: 'ja',
-		mailAddress: 'general@nulab.example'
-	}
+  {
+    id: 1,
+    userId: 'admin',
+    name: 'admin',
+    roleType: 1,
+    lang: 'ja',
+    mailAddress: 'admin@nulab.example'
+  },
+  {
+    id: 2,
+    userId: 'general',
+    name: 'general',
+    roleType: 2,
+    lang: 'ja',
+    mailAddress: 'general@nulab.example'
+  }
 ]
 
 export const USER_LIST_TABLE_HEADERS: TableHeader[] = [
-	{
-		text: 'id',
-		width: '25%',
-		fontSize: '18px'
-	},
-	{
-		text: 'Name',
-		width: '25%',
-		fontSize: '18px'
-	},
-	{
-		text: 'Mail',
-		width: '50%',
-		fontSize: '18px'
-	}
+  {
+    text: 'id',
+    width: '25%',
+    fontSize: '18px'
+  },
+  {
+    text: 'Name',
+    width: '25%',
+    fontSize: '18px'
+  },
+  {
+    text: 'Mail',
+    width: '50%',
+    fontSize: '18px'
+  }
 ]
